766a91f9c605a7a96c7529fb78d5495a61f2a6e7
Sflln <bikbaeva.ns@mail.ru>
2023-07-29 22:27:19 +0300

    3

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ function Card({ card, onCardClick }) {
     <div id="card">
       <div className="element">
         <button className="element__button-delete" type="button" aria-label="удалить" />
-        <img src={card.link} alt="картинка" className="element__img" onClick={handleClick} />
+        <img src={card.link} alt={card.name} className="element__img" onClick={handleClick} />
         <div className="element__unit">
           <h2 className="element__title">{card.name}</h2>
           <div className="element__unit element__unit_like">
